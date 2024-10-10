39b13984d94e820eac572928c0a8d1aff92450f8
krika1 <kristian.budov@mentormate.com>
2024-10-10 18:56:27 +0300

    TU-16: Added authorization

--- TinyUrl.AuthenticationService/TinyUrl.AuthenticationService/Controllers/AuthenticationController.cs
+++ TinyUrl.AuthenticationService/TinyUrl.AuthenticationService/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TinyUrl.AuthenticationService.Infrastructure.Common;
 using TinyUrl.AuthenticationService.Infrastructure.Contracts.Requests;
 using TinyUrl.AuthenticationService.Infrastructure.Contracts.Responses;
